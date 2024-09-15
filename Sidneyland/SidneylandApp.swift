6004a9aefca0fa551aad44214fafb88a57a52e4a
thomaslebonnec <thomaslebonnec@icloud.com>
2024-09-15 02:08:43 +0200

    Initial Commit

---
+++ Sidneyland/SidneylandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidneylandApp.swift
+//  Sidneyland
+//
+//  Created by Thomas Le Bonnec on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SidneylandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SidneylandTests/SidneylandTests.swift
@@ -0,0 +1,16 @@
+//
+//  SidneylandTests.swift
+//  SidneylandTests
+//
+//  Created by Thomas Le Bonnec on 15/09/2024.
+//
+
+import Testing
+
+struct SidneylandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
